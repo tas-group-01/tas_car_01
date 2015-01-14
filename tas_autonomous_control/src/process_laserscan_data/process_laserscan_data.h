fc7148aec3c7e3668650f8f82b4147ea63e09caa
Igor Krawczuk <igor.krawczuk@tum.de>
2015-01-14 12:16:13 +0100

    scanlaser

---
+++ tas_autonomous_control/src/process_laserscan_data/process_laserscan_data.h
@@ -0,0 +1,18 @@
+#ifndef PROCESS_LASERSCAN_DATA_H_GUARD
+#define PROCESS_LASERSCAN_DATA_H_GUARD
+
+#include "process_laserscan_data.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ios>
+#include <cstdio>
+#include <sstream>
+
+using namespace std;
+
+void laserscan_data_write(std::vector<float> data,int length);
+
+#endif
+
